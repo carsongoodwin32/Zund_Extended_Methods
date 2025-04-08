6f7031a448c7917fbe61d43fc4e7835bc50474e7
Carson Goodwin <carson.goodwin32@gmail.com>
2025-04-08 13:58:50 -0500

    environment_report.txt now gets generated (with incorrect data)

--- imports/testHandler.py
+++ imports/testHandler.py
@@ -1,11 +1,68 @@
+import os
+import datetime
+
+def timestamp_as_string():
+    timestamp = datetime.datetime.now().strftime("%m-%d_%H-%M-%S")
+    return f"[{timestamp}] "
+
 def exportEnvironmentReport(basedir,testResults):
-    return True
+    testStrings = [
+    "Writing to basedir", "Reading from basedir", "Deleting in basedir",
+    "Writing to logfile", "Reading from logfile", "Deleting logfile",
+    "Writing to logdir", "Reading from logdir", "Deleting in logdir",
+    "Writing to hotfolder", "Reading from hotfolder", "Deleting in hotfolder",
+    "Writing to output_dir", "Reading from output_dir", "Deleting in output_dir",
+    "Writing to original_files_dir", "Reading from original_files_dir", "Deleting in original_files_dir"
+    ]
+
+    if len(testResults) < len(testStrings):
+        print("Some tests not run! Exiting due to critical error!")
+        return False
+    if None in testResults:
+        print("Some tests skipped due to 'META' config settings!")
+
+    #Output the file here now that we've shown all warnings and we know we have all test results
+    env_file = basedir+os.sep+"environment_report.txt"
+
+    try:
+        f = open(env_file, "w")
+    except Exception as e:
+        print("Could not open "+env_file+" for writing. Recieved Exception: "+str(e)+". Exiting due to critical error!")
+        return False
+    
+    try:
+        for i in range(len(testStrings)):
+            res = "N/A"
+            if testResults[i] == True:
+                res = "Passed"
+            elif testResults[i] == False:
+                res = "Failed"
+            else:
+                res = "Skipped"
+
+            f.write(timestamp_as_string()+testStrings[i]+" - "+res+'\n')
+    except Exception as e:
+        print("Could not write to file "+env_file+". Recieved Exception: "+str(e)+". Exiting due to critical error!")
+        return False
+
+    try:
+        f.close()
+    except Exception as e:
+        print("Could not close "+env_file+" after writing. Recieved Exception: "+str(e)+". Exiting due to critical error!")
+        return False
+
+    if False in testResults:
+        print("Some tests failed! Check environment_report.txt for details! Exiting due to critical error!")
+        return False
+    else:
+        print("All run tests passed successfully! Continuing...")
+        return True
 
 def rwdTestAtPath(path):
-    return [True]
+    return [True,True,True]
 
 def testLogDir(path):
-    return path,[True]
+    return path,[True,True,True]
 
 def initTests(basedir,metaConfig):
     testResults = []
@@ -31,7 +88,7 @@ def initTests(basedir,metaConfig):
     #Reading, Writing, Deleting from the output dir
     testResults.extend(rwdTestAtPath(metaConfig.oD))
 
-    if(metaConfig.dFAP):
+    if(not metaConfig.dFAP):
         #Reading, Writing, Deleting from the original files dir
         testResults.extend(rwdTestAtPath(metaConfig.oFD))
     else:
